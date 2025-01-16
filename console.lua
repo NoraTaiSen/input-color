4df7c993c774c9dac2111b7dc6bf6d80ff23758e
Norataisen <142599223+NoraTaiSen@users.noreply.github.com>
2025-01-16 13:35:14 +0700

    Update console.lua

--- console.lua
+++ console.lua
@@ -1,36 +1,15 @@
-local Modules = {
+local Lib = {
     Colors = {
-        ["Green"] = "0,255,0", 
-        ["Cyan"] = "33, 161, 163",
-        ["White"] = "255,255,255",
-        ["Pink"] = "255, 102, 153",
-        ["Red"] = "255,0,0", 
-        ["Blue"] = "0,0,255", 
-        ["Yellow"] = "255,255,0", 
-        ["Orange"] = "255,165,0", 
-        ["Purple"] = "128,0,128", 
-        ["Brown"] = "139,69,19", 
-        ["Gray"] = "169,169,169", 
-        ["Black"] = "0,0,0", 
-        ["Lime"] = "0,255,0", 
-        ["Gold"] = "255,215,0", 
-        ["Silver"] = "192,192,192", 
-        ["Indigo"] = "75,0,130", 
-        ["Teal"] = "0,128,128", 
-        ["Violet"] = "238,130,238"
-    },
-    
-    Services = {
-        RunService = game:GetService("RunService"),
-        CoreGui = game:GetService("CoreGui")
+        ["Green"] = "0,255,0", -- color in rgb
+        ["Cyan"] = "33, 161, 163"
     }
 }
 
 -- Function to change colors in the DevConsole
-Modules.ChangeColor = function() 
-    Lib.Services.RunService.Heartbeat:Connect(function()
-        if Lib.Services.CoreGui:FindFirstChild("DevConsoleMaster") then 
-            for _, v in pairs(Lib.Services.CoreGui:FindFirstChild("DevConsoleMaster"):GetDescendants()) do 
+Lib.ChangeColor = function() 
+    game:GetService("RunService").Heartbeat:Connect(function()
+        if game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster") then 
+            for _, v in pairs(game:GetService("CoreGui"):FindFirstChild("DevConsoleMaster"):GetDescendants()) do 
                 if v:IsA("TextLabel") then 
                     v.RichText = true 
                 end 
@@ -39,42 +18,13 @@ Modules.ChangeColor = function()
     end)
 end
 
--- Function to create a loading bar with specified watermark and color
-Modules.LoadingBar = function(watermark, color, delay, loadingsymbol)
-    delay = delay or 0.1 
-    local Text = watermark .. tostring(math.random(500, 20000))
-    print(Text)
-
-    local loadingLabel = nil
-    local progress = ""
-
-    repeat task.wait()
-        for _, label in pairs(Lib.Services.CoreGui:FindFirstChild("DevConsoleMaster"):GetDescendants()) do 
-            if label:IsA("TextLabel") and string.find(label.Text:lower(), Text:lower()) then 
-                loadingLabel = label 
-                break
-            end 
-        end 
-    until loadingLabel
-
-    local start = os.time()
-
-    for i = 1, 50 do
-        progress = progress .. loadingsymbol
-        loadingLabel.Text = string.format("<font color='rgb(%s)' size='15'>[%s] [%d%% loaded] %s</font>", Lib.Colors["White"], watermark, i * 2, progress)
-        task.wait(delay)
-    end
-
-    loadingLabel.Text = string.format("<font color='rgb(%s)' size='15'>[%s] Successfully loaded in %ds</font>", Lib.Colors[color], watermark, os.time() - start)
-end
-
 -- Function to print colored text to the console
-Modules.print = function(color, text, size)
+Lib.print = function(color, text, size)
     if not Lib.Colors[color] then 
         warn("Color was not found!")
         return 
     end
-
+    
     local Text = '<font color="rgb(' .. Lib.Colors[color] .. ')"'
     if size then
         Text = Text .. ' size="' .. tostring(size) .. '"'
@@ -83,4 +33,8 @@ Modules.print = function(color, text, size)
     print(Text)
 end
 
-return Modules
+-- Call the ChangeColor function to initialize the color change
+Lib.ChangeColor()
+
+-- Return the Lib table so it can be used by other scripts
+return Lib
